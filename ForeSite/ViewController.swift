5692dd50218fb0367dd63922c22dac7d17a0ddf5
Bhargava Ramisetty <bhargava.ramisetty@gmail.com>
2018-12-09 11:53:01 -0800

    Initial Commit

---
+++ ForeSite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForeSite
+//
+//  Created by Bhargava on 12/9/18.
+//  Copyright © 2018 Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
