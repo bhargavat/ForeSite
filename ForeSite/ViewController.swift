73830b8f8072c6a361b767a7bf2a4b5f001f52cf
Bhargava Ramisetty <bhargava.ramisetty@gmail.com>
2019-01-03 00:56:57 -0800

    AWS setup + Auth functionality
    Finished adding AWS dependencies to project and having AWS authentication UI & functionality integrated with custom logo at login

--- ForeSite/ViewController.swift
+++ ForeSite/ViewController.swift
@@ -19,27 +19,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         AWSMobileClient.sharedInstance().initialize { (userState, error) in
             if let userState = userState {
-                switch(userState){
-                case .signedIn:
-                    DispatchQueue.main.async {
-                        self.signInStateLabel.text = "Logged In"
-                    }
-                case .signedOut:
-                    AWSMobileClient.sharedInstance().showSignIn(navigationController: self.navigationController!, { (userState, error) in
-                        if(error == nil){       //Successful signin
-                            DispatchQueue.main.async {
-                                self.signInStateLabel.text = "Logged In"
-                            }
-                        }
-                    })
-                default:
-                    AWSMobileClient.sharedInstance().signOut()
-                }
-                
+                print("UserState: \(userState.rawValue)")
             } else if let error = error {
-                print(error.localizedDescription)
+                print("error: \(error.localizedDescription)")
             }
         }
         
@@ -47,10 +32,65 @@ class ViewController: UIViewController {
             .showSignIn(navigationController: self.navigationController!,
                         signInUIOptions: SignInUIOptions(
                             canCancel: false,
-                            logoImage: UIImage(named: "MyCustomLogo"),
+                            logoImage: UIImage(named: "foresite-icon-1024.png"),
                             backgroundColor: UIColor.black)) { (result, err) in
                                 //handle results and errors
         }
+        
+        AWSMobileClient.sharedInstance().signUp(username: "your_username",
+                                                password: "Abc@123!",
+                                                userAttributes: ["email":"john@doe.com", "phone_number": "+1973123456"]) { (signUpResult, error) in
+            if let signUpResult = signUpResult {
+                switch(signUpResult.signUpConfirmationState) {
+                case .confirmed:
+                    print("User is signed up and confirmed.")
+                case .unconfirmed:
+                    print("User is not confirmed and needs verification via \(signUpResult.codeDeliveryDetails!.deliveryMedium) sent at \(signUpResult.codeDeliveryDetails!.destination!)")
+                case .unknown:
+                    print("Unexpected case")
+                }
+            } else if let error = error {
+                if let error = error as? AWSMobileClientError {
+                    switch(error) {
+                    case .usernameExists(let message):
+                        print(message)
+                    default:
+                        break
+                    }
+                }
+                print("\(error.localizedDescription)")
+            }
+        }
+        
+        AWSMobileClient.sharedInstance().signIn(username: "your_username", password: "Abc@123!") { (signInResult, error) in
+            if let error = error  {
+                print("\(error.localizedDescription)")
+            } else if let signInResult = signInResult {
+                switch (signInResult.signInState) {
+                case .signedIn:
+                    print("User is signed in.")
+                case .smsMFA:
+                    print("SMS message sent to \(signInResult.codeDetails!.destination!)")
+                default:
+                    print("Sign In needs info which is not et supported.")
+                }
+            }
+        }
+        
+        AWSMobileClient.sharedInstance().confirmSignIn(challengeResponse: "code_here") { (signInResult, error) in
+            if let error = error  {
+                print("\(error.localizedDescription)")
+            } else if let signInResult = signInResult {
+                switch (signInResult.signInState) {
+                case .signedIn:
+                    print("User is signed in.")
+                default:
+                    print("\(signInResult.signInState.rawValue)")
+                }
+            }
+        }
+        
+        AWSMobileClient.sharedInstance().signOut()
     }
 }
 
--- Podfile
+++ Podfile
@@ -5,11 +5,11 @@ target 'ForeSite' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  pod 'AWSCore', '~> 2.8.0'
+  pod 'AWSCore', '~> 2.8.1'
   pod 'AWSAppSync', '~> 2.8.0'
-  pod 'AWSMobileClient', '~> 2.8.0'      # Required dependency
-  pod 'AWSAuthUI', '~> 2.8.0'            # Optional dependency required to use drop-in UI
-  pod 'AWSUserPoolsSignIn', '~> 2.8.0'   # Optional dependency required to use drop-in UI
+  pod 'AWSMobileClient', '~> 2.8.1'      # Required dependency
+  pod 'AWSAuthUI', '~> 2.8.1'            # Optional dependency required to use drop-in UI
+  pod 'AWSUserPoolsSignIn', '~> 2.8.1'   # Optional dependency required to use drop-in UI
   # Pods for ForeSite
 
   target 'ForeSiteTests' do
